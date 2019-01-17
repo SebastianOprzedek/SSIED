d276364f592a76a92da8cb86e84bb85061602c4f
Marcin Bury <bmarcin94@gmail.com>
2019-01-17 22:12:01 +0100

    Added metrics and results printing

--- demo/text-classification.py
+++ demo/text-classification.py
@@ -29,13 +29,13 @@ def decode_review(text):
 	
 ### Our code
 
-print("Training entries length: {}, labels length: {}".format(len(train_data), len(train_labels)))
-print("Test entries length: {}, labels length: {}".format(len(test_data), len(test_labels)))
-print("Example record")
-print("raw: {}".format(train_data[0]))
-print("as word: {}".format(decode_review(train_data[0])))
-print("label: {}".format(train_labels[0]))
-print("Legend: 0 is a negative review, and 1 is a positive review")
+# print("Training entries length: {}, labels length: {}".format(len(train_data), len(train_labels)))
+# print("Test entries length: {}, labels length: {}".format(len(test_data), len(test_labels)))
+# print("Example record")
+# print("raw: {}".format(train_data[0]))
+# print("as word: {}".format(decode_review(train_data[0])))
+# print("label: {}".format(train_labels[0]))
+# print("Legend: 0 is a negative review, and 1 is a positive review")
 
 
 ### Data preprocessing
@@ -48,7 +48,32 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data,
                                                        padding='post',
                                                        maxlen=256)
 
-													   
+### Metryki - ocena sieci
+K = keras.backend
+
+## Precyzja
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+## Czułość
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+## Specyficzność
+def specificity(y_pred, y_true):
+    neg_y_true = 1 - y_true
+    neg_y_pred = 1 - y_pred
+    false_positive = K.sum(neg_y_true * y_pred)
+    true_negative = K.sum(neg_y_true * neg_y_pred)
+    specificity = true_negative / (true_negative + false_positive + K.epsilon())
+    return specificity
+
 ### Model
 vocab_size = 10000
 model = keras.Sequential()
@@ -59,7 +84,7 @@ model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 model.summary()
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy',precision, recall, specificity])
 
 			  
 ### Training and testing			  
@@ -77,4 +102,15 @@ history = model.fit(partial_x_train,
                     verbose=1)
 					
 results = model.evaluate(test_data, test_labels)
-print(results)
+
+### Miary jakości
+loss = round(results[0]*100,2)
+acc = round(results[1]*100,2)
+prec = round(results[2]*100,2)
+rec = round(results[3]*100,2)
+spec = round(results[4]*100,2)
+print('\nLoss: ', loss, '%') # strata
+print('Accuracy: ', acc, '%') # dokładność
+print('Precision: ', prec, '%') # precyzja
+print('Recall: ', rec, '%') # czułość
+print('Specificity: ', spec, '%') # specyficzność
